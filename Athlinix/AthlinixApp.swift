57661fa68fe4d9b0f9e97454b9bb178fafca3e64
Iamalsauser <parthsinhv4@gmail.com>
2024-10-13 00:19:57 +0530

    Initial Commit

---
+++ Athlinix/AthlinixApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthlinixApp.swift
+//  Athlinix
+//
+//  Created by admin17 on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AthlinixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AthlinixTests/AthlinixTests.swift
@@ -0,0 +1,17 @@
+//
+//  AthlinixTests.swift
+//  AthlinixTests
+//
+//  Created by admin17 on 13/10/24.
+//
+
+import Testing
+@testable import Athlinix
+
+struct AthlinixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
