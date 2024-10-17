4d5cc884e5de38326b4a9b224e6fb987d2e9778a
Iamalsauser <parthsinhv4@gmail.com>
2024-10-17 14:00:32 +0530

    update

---
+++ Athlinix/Preview Content/home/rosestat.swift
@@ -0,0 +1,18 @@
+//
+//  rosestat.swift
+//  Athlinix
+//
+//  Created by admin17 on 17/10/24.
+//
+
+import SwiftUI
+
+struct rosestat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    rosestat()
+}
